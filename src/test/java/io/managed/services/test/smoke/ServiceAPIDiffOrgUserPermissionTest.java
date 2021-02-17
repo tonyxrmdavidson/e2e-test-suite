b423c9359fe4a43757574222fe2f607a52314b14
Davide Bizzarri <dbizzarr@redhat.com>
2021-02-17 18:45:36 +0100

    refactor: split testCreateAndListKafkaInstance in multiple tests

--- src/test/java/io/managed/services/test/smoke/ServiceAPIDiffOrgUserPermissionTest.java
+++ src/test/java/io/managed/services/test/smoke/ServiceAPIDiffOrgUserPermissionTest.java
@@ -5,7 +5,6 @@ import io.managed.services.test.TestBase;
 import io.managed.services.test.client.ResponseException;
 import io.managed.services.test.client.kafka.KafkaAdmin;
 import io.managed.services.test.client.kafka.KafkaUtils;
-import io.managed.services.test.client.oauth.KeycloakOAuth;
 import io.managed.services.test.client.serviceapi.CreateKafkaPayload;
 import io.managed.services.test.client.serviceapi.CreateServiceAccountPayload;
 import io.managed.services.test.client.serviceapi.KafkaListResponse;
@@ -16,10 +15,8 @@ import io.managed.services.test.client.serviceapi.ServiceAccount;
 import io.managed.services.test.framework.TestTag;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.ext.auth.User;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
 import org.apache.kafka.common.errors.SaslAuthenticationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -40,7 +37,6 @@ import java.util.stream.Collectors;
 
 import static io.managed.services.test.TestUtils.await;
 import static io.managed.services.test.client.serviceapi.ServiceAPIUtils.deleteServiceAccountByNameIfExists;
-import static io.managed.services.test.client.serviceapi.ServiceAPIUtils.getKafkaByName;
 import static io.managed.services.test.client.serviceapi.ServiceAPIUtils.waitUntilKafkaIsReady;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
@@ -57,30 +53,19 @@ class ServiceAPIDiffOrgUserPermissionTest extends TestBase {
     static final String KAFKA_INSTANCE_NAME = "mk-e2e-dup-" + Environment.KAFKA_POSTFIX_NAME;
     static final String SERVICE_ACCOUNT_NAME_ORG2 = "mk-e2e-dup-sa-" + Environment.KAFKA_POSTFIX_NAME;
 
-    User userOfOrg1, userOfOrg2;
-    KeycloakOAuth auth;
-    ServiceAPI apiOrg1, apiOrg2;
+    ServiceAPI apiOrg1;
+    ServiceAPI apiOrg2;
 
-    boolean kafkaInstanceCreated = false;
+    // Kafka instance in Org1
+    KafkaResponse kafka;
 
     @BeforeAll
-    void bootstrap(Vertx vertx, VertxTestContext context) {
-        this.auth = new KeycloakOAuth(vertx,
-                Environment.SSO_REDHAT_KEYCLOAK_URI,
-                Environment.SSO_REDHAT_REDIRECT_URI,
-                Environment.SSO_REDHAT_REALM,
-                Environment.SSO_REDHAT_CLIENT_ID);
-
+    void bootstrap(Vertx vertx) {
         LOGGER.info("authenticate user: {} against: {}", Environment.SSO_USERNAME, Environment.SSO_REDHAT_KEYCLOAK_URI);
-        User userOfOrg1 = await(auth.login(Environment.SSO_USERNAME, Environment.SSO_PASSWORD));
-        User userOfOrg2 = await(auth.login(Environment.SSO_ALIEN_USERNAME, Environment.SSO_ALIEN_PASSWORD));
+        this.apiOrg1 = await(ServiceAPIUtils.serviceAPI(vertx, Environment.SSO_USERNAME, Environment.SSO_PASSWORD));
 
-        this.userOfOrg1 = userOfOrg1;
-        this.userOfOrg2 = userOfOrg2;
-        this.apiOrg1 = new ServiceAPI(vertx, Environment.SERVICE_API_URI, userOfOrg1);
-        this.apiOrg2 = new ServiceAPI(vertx, Environment.SERVICE_API_URI, userOfOrg2);
-
-        context.completeNow();
+        LOGGER.info("authenticate user: {} against: {}", Environment.SSO_ALIEN_USERNAME, Environment.SSO_REDHAT_KEYCLOAK_URI);
+        this.apiOrg2 = await(ServiceAPIUtils.serviceAPI(vertx, Environment.SSO_ALIEN_USERNAME, Environment.SSO_ALIEN_PASSWORD));
     }
 
     @AfterAll
@@ -93,15 +78,21 @@ class ServiceAPIDiffOrgUserPermissionTest extends TestBase {
         await(deleteServiceAccountByNameIfExists(apiOrg2, SERVICE_ACCOUNT_NAME_ORG2));
     }
 
-    /**
-     * Create a new Kafka instance in organization 1
-     * Test it should be there for user of organization 1
-     * And not available for user of another org
-     */
+    void assertAPI() {
+        assumeTrue(apiOrg1 != null, "apiOrg1 is null because the bootstrap has failed");
+        assumeTrue(apiOrg2 != null, "apiOrg2 is null because the bootstrap has failed");
+    }
+
+    void assertKafka() {
+        assumeTrue(kafka != null, "kafka is null because the testUser1CreateKafkaInstance has failed to create the Kafka instance");
+    }
+
+
     @Test
-    @Timeout(10 * 60 * 1000)
+    @Timeout(value = 15, timeUnit = TimeUnit.MINUTES)
     @Order(1)
-    void testCreateAndListKafkaInstance(Vertx vertx) {
+    void testOrg1UserCreateKafkaInstance(Vertx vertx) {
+        assertAPI();
 
         // Create Kafka Instance in org 1
         CreateKafkaPayload kafkaPayload = new CreateKafkaPayload();
@@ -112,41 +103,45 @@ class ServiceAPIDiffOrgUserPermissionTest extends TestBase {
         kafkaPayload.region = "us-east-1";
 
         LOGGER.info("create kafka instance in organisation 1: {}", kafkaPayload.name);
-        KafkaResponse kafka = await(apiOrg1.createKafka(kafkaPayload, true));
-        String kafkaIDOrg1 = kafka.id;
+        kafka = await(apiOrg1.createKafka(kafkaPayload, true));
+
+        // Wait until kafka goes to ready state
+        await(waitUntilKafkaIsReady(vertx, apiOrg1, kafka.id));
+    }
+
+    @Test
+    @Order(2)
+    void testOrg2UserListKafkaInstances() {
+        assertKafka();
 
         // Get list of kafka Instance in org 1 and test it should be there
         KafkaListResponse kafkaListInOrg1 = await(apiOrg1.getListOfKafkas());
         LOGGER.info("fetch list of instance in organisation 1 for user 1");
-        List<KafkaResponse> kafkaResponsesInOrg1 = kafkaListInOrg1.items.stream().filter(k -> k.id.equals(kafkaIDOrg1)).collect(Collectors.toList());
+        List<KafkaResponse> kafkaResponsesInOrg1 = kafkaListInOrg1.items.stream()
+                .filter(k -> k.id.equals(kafka.id)).collect(Collectors.toList());
 
         assertEquals(1, kafkaResponsesInOrg1.size(), "Kafka is present in the organisation 1");
-        LOGGER.info("Kafka {} is visible to Org 1", kafkaIDOrg1);
+        LOGGER.info("Kafka {} is visible to Org1 User", kafka.id);
 
         // Get list of kafka Instance in org 2 and test it should not be there
         LOGGER.info("fetch list of instance of organisation 2");
         KafkaListResponse kafkaListInOrg2 = await(apiOrg2.getListOfKafkas());
-        List<KafkaResponse> kafkaResponsesInOrg2 = kafkaListInOrg2.items.stream().filter(k -> k.id.equals(kafkaIDOrg1)).collect(Collectors.toList());
+        List<KafkaResponse> kafkaResponsesInOrg2 = kafkaListInOrg2.items.stream()
+                .filter(k -> k.id.equals(kafka.id)).collect(Collectors.toList());
+
         assertEquals(0, kafkaResponsesInOrg2.size(), "Kafka is not present in organisation 2");
-        LOGGER.info("Kafka {} is not visible to Org 2", kafkaIDOrg1);
-
-        // Wait until kafka goes to ready state
-        await(waitUntilKafkaIsReady(vertx, apiOrg1, kafkaIDOrg1));
-
-        kafkaInstanceCreated = true;
+        LOGGER.info("Kafka {} is not visible to Org2 User", kafka.id);
     }
 
     /**
      * A user in org A is not allowed to create topic to produce and consume messages on a kafka instance in org B
      */
     @Test
+    @Timeout(value = 5, timeUnit = TimeUnit.MINUTES)
     @Order(2)
     @Disabled("Known issue: https://issues.redhat.com/browse/MGDSTRM-1439")
-    @Timeout(value = 5, timeUnit = TimeUnit.MINUTES)
-    void testCreateTopicInOrg1KafkaByOrg2() {
-        assumeTrue(kafkaInstanceCreated, "testCreateAndListKafkaInstance failed");
-
-        var kafka = await(getKafkaByName(apiOrg1, KAFKA_INSTANCE_NAME)).orElseThrow();
+    void testOrg2UserCreateTopic() {
+        assertKafka();
 
         // Create Service Account of Org 2
         CreateServiceAccountPayload serviceAccountPayload = new CreateServiceAccountPayload();
@@ -180,16 +175,12 @@ class ServiceAPIDiffOrgUserPermissionTest extends TestBase {
                 }));
     }
 
-    /**
-     * Create a new Kafka instance in organization 1
-     */
+
     @Test
     @Timeout(value = 10, timeUnit = TimeUnit.MINUTES)
     @Order(3)
-    void deleteKafkaOfOrg1ByOrg2() {
-        assumeTrue(kafkaInstanceCreated, "testCreateAndListKafkaInstance failed");
-
-        var kafka = await(getKafkaByName(apiOrg1, KAFKA_INSTANCE_NAME)).orElseThrow();
+    void testOrg2UserDeleteKafkaInstance() {
+        assertKafka();
 
         LOGGER.info("Delete Instance: {} of Org 1 using user of Org 2", kafka.id);
         await(apiOrg2.deleteKafka(kafka.id, true)
