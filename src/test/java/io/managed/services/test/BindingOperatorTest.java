816dbada83d0ba3300219025f8fd84a9631a9c56
Davide Bizzarri <dbizzarr@redhat.com>
2021-03-16 19:14:53 +0100

    Update rhoas-model to operator 0.5.0

--- src/test/java/io/managed/services/test/BindingOperatorTest.java
+++ src/test/java/io/managed/services/test/BindingOperatorTest.java
@@ -1,13 +1,13 @@
 package io.managed.services.test;
 
 
+import com.openshift.cloud.v1alpha.models.CloudServiceAccountRequest;
+import com.openshift.cloud.v1alpha.models.CloudServiceAccountRequestSpec;
+import com.openshift.cloud.v1alpha.models.CloudServicesRequest;
+import com.openshift.cloud.v1alpha.models.CloudServicesRequestSpec;
 import com.openshift.cloud.v1alpha.models.Credentials;
-import com.openshift.cloud.v1alpha.models.ManagedKafkaConnection;
-import com.openshift.cloud.v1alpha.models.ManagedKafkaConnectionSpec;
-import com.openshift.cloud.v1alpha.models.ManagedServiceAccountRequest;
-import com.openshift.cloud.v1alpha.models.ManagedServiceAccountRequestSpec;
-import com.openshift.cloud.v1alpha.models.ManagedServicesRequest;
-import com.openshift.cloud.v1alpha.models.ManagedServicesRequestSpec;
+import com.openshift.cloud.v1alpha.models.KafkaConnection;
+import com.openshift.cloud.v1alpha.models.KafkaConnectionSpec;
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
@@ -66,11 +66,11 @@ public class BindingOperatorTest extends TestBase {
     KubernetesClient client;
 
     final static String ACCESS_TOKEN_SECRET_NAME = "mk-e2e-api-accesstoken";
-    final static String MANAGED_SERVICE_ACCOUNT_REQUEST_NAME = "mk-e2e-service-account-request";
+    final static String CLOUD_SERVICE_ACCOUNT_REQUEST_NAME = "mk-e2e-service-account-request";
     final static String SERVICE_ACCOUNT_NAME = "mk-e2e-service-account";
     final static String SERVICE_ACCOUNT_SECRET_NAME = "mk-e2e-service-account-secret";
-    final static String MANAGED_KAFKA_REQUEST_NAME = "mk-e2e-kafka-request";
-    final static String MANAGED_KAFKA_CONNECTION_NAME = "mk-e2e-kafka-connection";
+    final static String CLOUD_SERVICES_REQUEST_NAME = "mk-e2e-kafka-request";
+    final static String KAFKA_CONNECTION_NAME = "mk-e2e-kafka-connection";
 
     @BeforeAll
     void bootstrap(Vertx vertx, VertxTestContext context) {
@@ -113,29 +113,29 @@ public class BindingOperatorTest extends TestBase {
     }
 
     @AfterAll
-    void cleanManagedServiceAccountRequest() {
-        ManagedServiceAccountRequest a = OperatorUtils.managedServiceAccountRequest(client).withName(MANAGED_SERVICE_ACCOUNT_REQUEST_NAME).get();
+    void cleanCloudServiceAccountRequest() {
+        var a = OperatorUtils.cloudServiceAccountRequest(client).withName(CLOUD_SERVICE_ACCOUNT_REQUEST_NAME).get();
         if (a != null) {
-            LOGGER.info("clean ManagedServiceAccountRequest: {}", a.getMetadata().getName());
-            OperatorUtils.managedServiceAccountRequest(client).delete(a);
+            LOGGER.info("clean CloudServiceAccountRequest: {}", a.getMetadata().getName());
+            OperatorUtils.cloudServiceAccountRequest(client).delete(a);
         }
     }
 
     @AfterAll
-    void cleanManagedServicesRequest() {
-        var k = OperatorUtils.managedServicesRequest(client).withName(MANAGED_KAFKA_REQUEST_NAME).get();
+    void cleanCloudServicesRequest() {
+        var k = OperatorUtils.cloudServicesRequest(client).withName(CLOUD_SERVICES_REQUEST_NAME).get();
         if (k != null) {
-            LOGGER.info("clean ManagedServicesRequest: {}", k.getMetadata().getName());
-            OperatorUtils.managedServicesRequest(client).delete(k);
+            LOGGER.info("clean CloudServicesRequest: {}", k.getMetadata().getName());
+            OperatorUtils.cloudServicesRequest(client).delete(k);
         }
     }
 
     @AfterAll
     void cleanManagedKafkaConnection() {
-        var c = OperatorUtils.managedKafkaConnection(client).withName(MANAGED_KAFKA_CONNECTION_NAME).get();
+        var c = OperatorUtils.kafkaConnection(client).withName(KAFKA_CONNECTION_NAME).get();
         if (c != null) {
             LOGGER.info("clean ManagedKafkaConnection: {}", c.getMetadata().getName());
-            OperatorUtils.managedKafkaConnection(client).delete(c);
+            OperatorUtils.kafkaConnection(client).delete(c);
         }
     }
 
@@ -164,28 +164,28 @@ public class BindingOperatorTest extends TestBase {
     @Test
     @Order(2)
     @Timeout(value = 5, timeUnit = TimeUnit.MINUTES)
-    void createManagedServiceAccountRequest(Vertx vertx, VertxTestContext context) {
+    void createCloudServiceAccountRequest(Vertx vertx, VertxTestContext context) {
         assumeTrue(client != null, "the global client is null");
 
-        var a = new ManagedServiceAccountRequest();
-        a.getMetadata().setName(MANAGED_SERVICE_ACCOUNT_REQUEST_NAME);
-        a.setSpec(new ManagedServiceAccountRequestSpec());
+        var a = new CloudServiceAccountRequest();
+        a.getMetadata().setName(CLOUD_SERVICE_ACCOUNT_REQUEST_NAME);
+        a.setSpec(new CloudServiceAccountRequestSpec());
         a.getSpec().setServiceAccountName(SERVICE_ACCOUNT_NAME);
         a.getSpec().setServiceAccountDescription("");
         a.getSpec().setServiceAccountSecretName(SERVICE_ACCOUNT_SECRET_NAME);
         a.getSpec().setAccessTokenSecretName(ACCESS_TOKEN_SECRET_NAME);
 
-        LOGGER.info("create ManagedServiceAccountRequest with name: {}", MANAGED_SERVICE_ACCOUNT_REQUEST_NAME);
-        a = OperatorUtils.managedServiceAccountRequest(client).create(a);
-        LOGGER.info("created ManagedServiceAccountRequest: {}", Json.encode(a));
+        LOGGER.info("create CloudServiceAccountRequest with name: {}", CLOUD_SERVICE_ACCOUNT_REQUEST_NAME);
+        a = OperatorUtils.cloudServiceAccountRequest(client).create(a);
+        LOGGER.info("created CloudServiceAccountRequest: {}", Json.encode(a));
 
-        IsReady<ManagedServiceAccountRequest> ready = last -> Future.succeededFuture(OperatorUtils.managedServiceAccountRequest(client).withName(MANAGED_SERVICE_ACCOUNT_REQUEST_NAME).get())
+        IsReady<CloudServiceAccountRequest> ready = last -> Future.succeededFuture(OperatorUtils.cloudServiceAccountRequest(client).withName(CLOUD_SERVICE_ACCOUNT_REQUEST_NAME).get())
                 .map(r -> {
 
-                    LOGGER.info("ManagedServiceAccountRequest status is: {}", Json.encode(r.getStatus()));
+                    LOGGER.info("CloudServiceAccountRequest status is: {}", Json.encode(r.getStatus()));
 
                     if (last) {
-                        LOGGER.warn("last ManagedServiceAccountRequest is: {}", Json.encode(r));
+                        LOGGER.warn("last CloudServiceAccountRequest is: {}", Json.encode(r));
                     }
 
                     if (r.getStatus() != null && r.getStatus().getMessage().equals("Created")) {
@@ -193,32 +193,32 @@ public class BindingOperatorTest extends TestBase {
                     }
                     return Pair.with(false, null);
                 });
-        waitFor(vertx, "ManagedServiceAccountRequest to complete", ofSeconds(10), ofMinutes(2), ready)
-                .onSuccess(r -> LOGGER.info("ManagedServiceAccountRequest is ready: {}", Json.encode(r)))
+        waitFor(vertx, "CloudServiceAccountRequest to complete", ofSeconds(10), ofMinutes(2), ready)
+                .onSuccess(r -> LOGGER.info("CloudServiceAccountRequest is ready: {}", Json.encode(r)))
                 .onComplete(context.succeedingThenComplete());
     }
 
     @Test
     @Order(2)
-    void createManagedServicesRequest(Vertx vertx, VertxTestContext context) {
+    void createCloudServicesRequest(Vertx vertx, VertxTestContext context) {
         assumeTrue(client != null, "the global client is null");
 
-        var k = new ManagedServicesRequest();
-        k.getMetadata().setName(MANAGED_KAFKA_REQUEST_NAME);
-        k.setSpec(new ManagedServicesRequestSpec());
+        var k = new CloudServicesRequest();
+        k.getMetadata().setName(CLOUD_SERVICES_REQUEST_NAME);
+        k.setSpec(new CloudServicesRequestSpec());
         k.getSpec().setAccessTokenSecretName(ACCESS_TOKEN_SECRET_NAME);
 
-        LOGGER.info("create ManagedServicesRequest with name: {}", MANAGED_KAFKA_REQUEST_NAME);
-        k = OperatorUtils.managedServicesRequest(client).create(k);
-        LOGGER.info("created ManagedServicesRequest: {}", Json.encode(k));
+        LOGGER.info("create CloudServicesRequest with name: {}", CLOUD_SERVICES_REQUEST_NAME);
+        k = OperatorUtils.cloudServicesRequest(client).create(k);
+        LOGGER.info("created CloudServicesRequest: {}", Json.encode(k));
 
-        IsReady<ManagedServicesRequest> ready = last -> Future.succeededFuture(OperatorUtils.managedServicesRequest(client).withName(MANAGED_KAFKA_REQUEST_NAME).get())
+        IsReady<CloudServicesRequest> ready = last -> Future.succeededFuture(OperatorUtils.cloudServicesRequest(client).withName(CLOUD_SERVICES_REQUEST_NAME).get())
                 .map(r -> {
 
-                    LOGGER.info("ManagedServicesRequest status is: {}", Json.encode(r.getStatus()));
+                    LOGGER.info("CloudServicesRequest status is: {}", Json.encode(r.getStatus()));
 
                     if (last) {
-                        LOGGER.warn("last ManagedServicesRequest is: {}", Json.encode(r));
+                        LOGGER.warn("last CloudServicesRequest is: {}", Json.encode(r));
                     }
 
                     if (r.getStatus() != null
@@ -229,8 +229,8 @@ public class BindingOperatorTest extends TestBase {
                     }
                     return Pair.with(false, null);
                 });
-        waitFor(vertx, "ManagedServicesRequest to complete", ofSeconds(10), ofMinutes(3), ready)
-                .onSuccess(r -> LOGGER.info("ManagedServicesRequest is ready: {}", Json.encode(r)))
+        waitFor(vertx, "CloudServicesRequest to complete", ofSeconds(10), ofMinutes(3), ready)
+                .onSuccess(r -> LOGGER.info("CloudServicesRequest is ready: {}", Json.encode(r)))
                 .onComplete(context.succeedingThenComplete());
     }
 
@@ -240,31 +240,31 @@ public class BindingOperatorTest extends TestBase {
     void createManagedKafkaConnection(Vertx vertx, VertxTestContext context) {
         assumeTrue(client != null, "the global client is null");
 
-        var managedServicesRequest = OperatorUtils.managedServicesRequest(client).withName(MANAGED_KAFKA_REQUEST_NAME).get();
-        assumeTrue(managedServicesRequest != null, "the ManagedServicesRequest is null");
-        assumeTrue(managedServicesRequest.getStatus() != null, "the ManagedServicesRequest status is null");
+        var cloudServicesRequest = OperatorUtils.cloudServicesRequest(client).withName(CLOUD_SERVICES_REQUEST_NAME).get();
+        assumeTrue(cloudServicesRequest != null, "the CloudServicesRequest is null");
+        assumeTrue(cloudServicesRequest.getStatus() != null, "the CloudServicesRequest status is null");
 
-        var userKafka = managedServicesRequest.getStatus().getUserKafkas().stream()
+        var userKafka = cloudServicesRequest.getStatus().getUserKafkas().stream()
                 .filter(k -> k.getName().equals(KAFKA_INSTANCE_NAME))
                 .findFirst();
 
         if (userKafka.isEmpty()) {
-            LOGGER.info("ManagedServicesRequest: {}", Json.encode(managedServicesRequest));
-            fail(String.format("failed to find the user kafka instance %s in the ManagedServicesRequest %s", KAFKA_INSTANCE_NAME, MANAGED_KAFKA_REQUEST_NAME));
+            LOGGER.info("CloudServicesRequest: {}", Json.encode(cloudServicesRequest));
+            fail(String.format("failed to find the user kafka instance %s in the CloudServicesRequest %s", KAFKA_INSTANCE_NAME, CLOUD_SERVICES_REQUEST_NAME));
         }
 
-        var c = new ManagedKafkaConnection();
-        c.getMetadata().setName(MANAGED_KAFKA_CONNECTION_NAME);
-        c.setSpec(new ManagedKafkaConnectionSpec());
+        var c = new KafkaConnection();
+        c.getMetadata().setName(KAFKA_CONNECTION_NAME);
+        c.setSpec(new KafkaConnectionSpec());
         c.getSpec().setAccessTokenSecretName(ACCESS_TOKEN_SECRET_NAME);
         c.getSpec().setKafkaId(userKafka.orElseThrow().getId());
         c.getSpec().setCredentials(new Credentials(SERVICE_ACCOUNT_SECRET_NAME));
 
-        LOGGER.info("create ManagedKafkaConnection with name: {}", MANAGED_KAFKA_CONNECTION_NAME);
-        c = OperatorUtils.managedKafkaConnection(client).create(c);
+        LOGGER.info("create ManagedKafkaConnection with name: {}", KAFKA_CONNECTION_NAME);
+        c = OperatorUtils.kafkaConnection(client).create(c);
         LOGGER.info("created ManagedKafkaConnection: {}", Json.encode(c));
 
-        IsReady<ManagedKafkaConnection> ready = last -> Future.succeededFuture(OperatorUtils.managedKafkaConnection(client).withName(MANAGED_KAFKA_CONNECTION_NAME).get())
+        IsReady<KafkaConnection> ready = last -> Future.succeededFuture(OperatorUtils.kafkaConnection(client).withName(KAFKA_CONNECTION_NAME).get())
                 .map(r -> {
 
                     LOGGER.info("ManagedKafkaConnection status is: {}", Json.encode(r.getStatus()));
