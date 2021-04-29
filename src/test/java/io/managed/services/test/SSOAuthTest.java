4a4b73db9b76bbf3ed83dca19ce40fbf112f5e24
Davide Bizzarri <dbizzarr@redhat.com>
2021-04-29 11:55:00 +0200

    Remove vertx junit from SSOAuthTest

--- src/test/java/io/managed/services/test/SSOAuthTest.java
+++ src/test/java/io/managed/services/test/SSOAuthTest.java
@@ -3,82 +3,74 @@ package io.managed.services.test;
 import io.managed.services.test.client.oauth.KeycloakOAuth;
 import io.managed.services.test.framework.TestTag;
 import io.vertx.core.Vertx;
-import io.vertx.junit5.Timeout;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.concurrent.TimeUnit;
 
+import static io.managed.services.test.TestUtils.bwait;
+
 
 @Tag(TestTag.SERVICE_API)
-@ExtendWith(VertxExtension.class)
-@Timeout(value = 5, timeUnit = TimeUnit.MINUTES)
+@Timeout(value = 5, unit = TimeUnit.MINUTES)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SSOAuthTest extends TestBase {
     private static final Logger LOGGER = LogManager.getLogger(SSOAuthTest.class);
 
     @Test
-    void testRedHatSSOLogin(Vertx vertx, VertxTestContext context) {
+    void testRedHatSSOLogin() throws Throwable {
 
-        var auth = new KeycloakOAuth(vertx);
+        var auth = new KeycloakOAuth(Vertx.vertx());
 
-        auth.login(
-                Environment.SSO_REDHAT_KEYCLOAK_URI,
-                Environment.SSO_REDHAT_REDIRECT_URI,
-                Environment.SSO_REDHAT_REALM,
-                Environment.SSO_REDHAT_CLIENT_ID,
-                Environment.SSO_USERNAME,
-                Environment.SSO_PASSWORD)
-                .onSuccess(user -> LOGGER.info("user authenticated against: {}", Environment.SSO_REDHAT_KEYCLOAK_URI))
-
-                .onComplete(context.succeedingThenComplete());
+        bwait(auth.login(
+            Environment.SSO_REDHAT_KEYCLOAK_URI,
+            Environment.SSO_REDHAT_REDIRECT_URI,
+            Environment.SSO_REDHAT_REALM,
+            Environment.SSO_REDHAT_CLIENT_ID,
+            Environment.SSO_USERNAME,
+            Environment.SSO_PASSWORD));
+        LOGGER.info("user authenticated against: {}", Environment.SSO_REDHAT_KEYCLOAK_URI);
     }
 
     @Test
-    void testMASSSOLogin(Vertx vertx, VertxTestContext context) {
-        var auth2 = new KeycloakOAuth(vertx);
+    void testMASSSOLogin() throws Throwable {
+        var auth2 = new KeycloakOAuth(Vertx.vertx());
 
-        auth2.login(
-                Environment.MAS_SSO_REDHAT_KEYCLOAK_URI,
-                Environment.MAS_SSO_REDHAT_REDIRECT_URI,
-                Environment.MAS_SSO_REDHAT_REALM,
-                Environment.MAS_SSO_REDHAT_CLIENT_ID,
-                Environment.SSO_USERNAME,
-                Environment.SSO_PASSWORD)
-                .onSuccess(user -> LOGGER.info("user authenticated against: {}", Environment.MAS_SSO_REDHAT_KEYCLOAK_URI))
-
-                .onComplete(context.succeedingThenComplete());
+        bwait(auth2.login(
+            Environment.MAS_SSO_REDHAT_KEYCLOAK_URI,
+            Environment.MAS_SSO_REDHAT_REDIRECT_URI,
+            Environment.MAS_SSO_REDHAT_REALM,
+            Environment.MAS_SSO_REDHAT_CLIENT_ID,
+            Environment.SSO_USERNAME,
+            Environment.SSO_PASSWORD));
+        LOGGER.info("user authenticated against: {}", Environment.MAS_SSO_REDHAT_KEYCLOAK_URI);
     }
 
     @Test
-    void testJoinedLogin(Vertx vertx, VertxTestContext context) {
-        var auth = new KeycloakOAuth(vertx);
+    void testJoinedLogin() throws Throwable {
+        var auth = new KeycloakOAuth(Vertx.vertx());
 
-        auth.login(
-                Environment.SSO_REDHAT_KEYCLOAK_URI,
-                Environment.SSO_REDHAT_REDIRECT_URI,
-                Environment.SSO_REDHAT_REALM,
-                Environment.SSO_REDHAT_CLIENT_ID,
-                Environment.SSO_USERNAME,
-                Environment.SSO_PASSWORD)
-                .onSuccess(user -> LOGGER.info("user authenticated against: {}", Environment.SSO_REDHAT_KEYCLOAK_URI))
+        bwait(auth.login(
+            Environment.SSO_REDHAT_KEYCLOAK_URI,
+            Environment.SSO_REDHAT_REDIRECT_URI,
+            Environment.SSO_REDHAT_REALM,
+            Environment.SSO_REDHAT_CLIENT_ID,
+            Environment.SSO_USERNAME,
+            Environment.SSO_PASSWORD));
+        LOGGER.info("user authenticated against: {}", Environment.SSO_REDHAT_KEYCLOAK_URI);
 
-                .compose(__ -> auth.login(
-                        Environment.MAS_SSO_REDHAT_KEYCLOAK_URI,
-                        Environment.MAS_SSO_REDHAT_REDIRECT_URI,
-                        Environment.MAS_SSO_REDHAT_REALM,
-                        Environment.MAS_SSO_REDHAT_CLIENT_ID,
-                        Environment.SSO_USERNAME,
-                        Environment.SSO_PASSWORD))
-                .onSuccess(user -> LOGGER.info("user authenticated against: {}", Environment.MAS_SSO_REDHAT_KEYCLOAK_URI))
-
-                .onComplete(context.succeedingThenComplete());
+        bwait(auth.login(
+            Environment.MAS_SSO_REDHAT_KEYCLOAK_URI,
+            Environment.MAS_SSO_REDHAT_REDIRECT_URI,
+            Environment.MAS_SSO_REDHAT_REALM,
+            Environment.MAS_SSO_REDHAT_CLIENT_ID,
+            Environment.SSO_USERNAME,
+            Environment.SSO_PASSWORD));
+        LOGGER.info("user authenticated against: {}", Environment.MAS_SSO_REDHAT_KEYCLOAK_URI);
     }
 }
