affcba6c7b3f61be458ee6acb6f16c3a6ca8fdcf
Davide Bizzarri <dbizzarr@redhat.com>
2021-05-03 14:02:52 +0200

    Migrate SmokeTest to TestNG

--- src/test/java/io/managed/services/test/SmokeTest.java
+++ src/test/java/io/managed/services/test/SmokeTest.java
@@ -1,31 +1,47 @@
 package io.managed.services.test;
 
-import io.managed.services.test.framework.TestTag;
+import io.managed.services.test.client.kafka.KafkaMessagingUtils;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.testng.Assert.assertNotNull;
 
 
-@Tag(TestTag.SMOKE)
+//@Tag(TestTag.SMOKE)
 public class SmokeTest extends TestBase {
     private static final Logger LOGGER = LogManager.getLogger(SmokeTest.class);
 
     @Test
-    void smokeTest() {
+    public void smokeTest() {
         assumeTrue(true);
     }
 
     @Test
-    void testBlockingWait() {
+    public void random() {
+        var r1 = KafkaMessagingUtils.random(0, 1);
+        LOGGER.info("r1: {}", r1);
+        assertTrue(r1 >= 0 && r1 <= 1, "r1 is not in range 0,1");
+
+        var r2 = KafkaMessagingUtils.random(10, 1000);
+        LOGGER.info("r2: {}", r2);
+        assertTrue(r2 >= 10 && r2 <= 1000, "r2 is not in range 10,1000");
+
+        var r3 = KafkaMessagingUtils.random(10, 10);
+        LOGGER.info("r3: {}", r3);
+        assertEquals(r3, 10, "r3 is not equal 10");
+    }
+
+    @Test
+    public void testBlockingWait() throws Throwable {
         var vertx = Vertx.vertx();
 
         var r = new AtomicReference<IllegalCallerException>();
@@ -43,9 +59,9 @@ public class SmokeTest extends TestBase {
         });
 
         // when used outside a vertx callback bwait should succeed
-        Assertions.assertDoesNotThrow(() -> TestUtils.bwait(p.future()));
+        TestUtils.bwait(p.future());
 
         // when used within a vertx callback bwait should fail
-        Assertions.assertNotNull(r.get(), "TestUtils.bwait should throw an IllegalCallerException");
+        assertNotNull(r.get(), "TestUtils.bwait should throw an IllegalCallerException");
     }
 }
