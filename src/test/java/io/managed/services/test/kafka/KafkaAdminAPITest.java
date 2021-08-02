9baf063b48ff688044f890a5c621319d11fe92d0
Davide Bizzarri <dbizzarr@redhat.com>
2021-08-02 17:19:34 +0200

    fix: use FAKE_TOKEN from the TestUtils

--- src/test/java/io/managed/services/test/kafka/KafkaAdminAPITest.java
+++ src/test/java/io/managed/services/test/kafka/KafkaAdminAPITest.java
@@ -3,6 +3,7 @@ package io.managed.services.test.kafka;
 import io.managed.services.test.Environment;
 import io.managed.services.test.IsReady;
 import io.managed.services.test.TestBase;
+import io.managed.services.test.TestUtils;
 import io.managed.services.test.client.exception.HTTPConflictException;
 import io.managed.services.test.client.exception.HTTPLockedException;
 import io.managed.services.test.client.exception.HTTPNotFoundException;
@@ -64,39 +65,6 @@ public class KafkaAdminAPITest extends TestBase {
     private KafkaResponse kafka;
     private KafkaConsumer<String, String> kafkaConsumer;
 
-    static final String FAKE_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUI" +
-            "iwia2lkIiA6ICItNGVsY19WZE5fV3NPVVlmMkc0UXhyOEdjd0l4X0t0WFVDaXRhd" +
-            "ExLbEx3In0.eyJleHAiOjE2MTM3MzI2NzAsImlhdCI6MTYxMzczMTc3MCwiYXV0a" +
-            "F90aW1lIjoxNjEzNzMxNzY5LCJqdGkiOiIyZjAzYjI4Ni0yNWEzLTQyZjItOTdlY" +
-            "S0zMjAwMjBjNWRkMzYiLCJpc3MiOiJodHRwczovL3Nzby5yZWRoYXQuY29tL2F1d" +
-            "GgvcmVhbG1zL3JlZGhhdC1leHRlcm5hbCIsImF1ZCI6ImNsb3VkLXNlcnZpY2VzI" +
-            "iwic3ViIjoiZjo1MjhkNzZmZi1mNzA4LTQzZWQtOGNkNS1mZTE2ZjRmZTBjZTY6b" +
-            "WstdGVzdC11c2VyLWUyZS1wcmltYXJ5IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiY" +
-            "2xvdWQtc2VydmljZXMiLCJzZXNzaW9uX3N0YXRlIjoiNWIzNzMzODktM2FhOC00Y" +
-            "jExLTg2MTctOGYwNDQwM2Y2OTE5IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2luc" +
-            "yI6WyJodHRwczovL3Byb2QuZm9vLnJlZGhhdC5jb206MTMzNyIsImh0dHBzOi8vY" +
-            "XBpLmNsb3VkLnJlZGhhdC5jb20iLCJodHRwczovL3FhcHJvZGF1dGguY2xvdWQuc" +
-            "mVkaGF0LmNvbSIsImh0dHBzOi8vY2xvdWQub3BlbnNoaWZ0LmNvbSIsImh0dHBzO" +
-            "i8vcHJvZC5mb28ucmVkaGF0LmNvbSIsImh0dHBzOi8vY2xvdWQucmVkaGF0LmNvb" +
-            "SJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiYXV0aGVudGljYXRlZCJdfSwic" +
-            "2NvcGUiOiIiLCJhY2NvdW50X251bWJlciI6IjcwMjQ0MDciLCJpc19pbnRlcm5hb" +
-            "CI6ZmFsc2UsImFjY291bnRfaWQiOiI1Mzk4ODU3NCIsImlzX2FjdGl2ZSI6dHJ1Z" +
-            "Swib3JnX2lkIjoiMTQwMTQxNjEiLCJsYXN0X25hbWUiOiJVc2VyIiwidHlwZSI6I" +
-            "lVzZXIiLCJsb2NhbGUiOiJlbl9VUyIsImZpcnN0X25hbWUiOiJUZXN0IiwiZW1ha" +
-            "WwiOiJtay10ZXN0LXVzZXIrZTJlLXByaW1hcnlAcmVkaGF0LmNvbSIsInVzZXJuY" +
-            "W1lIjoibWstdGVzdC11c2VyLWUyZS1wcmltYXJ5IiwiaXNfb3JnX2FkbWluIjpmY" +
-            "WxzZX0.y0OHnHA8wLKPhpoeBp_8V4r76R6Miqdj6fNevWHOBsrJ4_j9GJJ2QfJme" +
-            "TSY5V3d0nT2Rt2SZ9trPrLEFd3Wr5z9YGIle--TXKKkYKyyFr4FO8Uaxvh-oN45C" +
-            "3cGsNYfbRBILqBCFHTmh54q1XoHA6FiteqdgMzUrBAoFG3SeFLl41u9abNA7EEe8" +
-            "0ldozXsiSaLDWSylF1g9u1BhGqGuOpX0RoZGuTL_3KINEE7XoCbvW0xKecCA8-u1" +
-            "C06X_GUgR0tVvdgoGpB9uPDX3sbqMpl7fNgJvwyZa8acVoJuxs5K945OYGzGXuDG" +
-            "Gzt-zxEov9g4udCDxNQTUoHuCIrMrr1ubt2iFbqso4UF6h-NIbxqARxhlhhyH8U9" +
-            "c2Zm1J_fLA9WJ8g1DJF75D66hV05s_RyRX1G6dFEriuT00PbGZQrxgH38zgZ8s-a" +
-            "S3qCAc2vYS-ZD4_Sl2xQgICC1HYpbgUbWNeAVEOWygZJUPMJLgpJ3aM2P8Dnjia5" +
-            "0KL0owSTYBWvFDkROI-ymDXfcRvEMVKyOdhljQNPZew4Ux4apBi9t-ncB9XabDo1" +
-            "1eddbbmcV05FWDb8X4opshptnWDzAw4ZPhbjoTBhNEI2JbFssOSYpskNnkB4kKQb" +
-            "BjVxAPldBNFwRKLOfvJNdY1jNurMY1xVMl2dbEpFBkqJf1lByU";
-
     // TODO: Test update topic with random values
 
     @BeforeClass(timeOut = 10 * MINUTES)
@@ -140,13 +108,13 @@ public class KafkaAdminAPITest extends TestBase {
         LOGGER.info("Test different organisation user");
         var bootstrapServerHost = kafka.bootstrapServerHost;
         var kafkaAdminAPIDifferentOrganization = bwait(KafkaAdminAPIUtils.kafkaAdminAPI(
-                vertx,
-                bootstrapServerHost,
-                Environment.SSO_ALIEN_USERNAME,
-                Environment.SSO_ALIEN_PASSWORD
+            vertx,
+            bootstrapServerHost,
+            Environment.SSO_ALIEN_USERNAME,
+            Environment.SSO_ALIEN_PASSWORD
         ));
         assertThrows(HTTPUnauthorizedException.class, () ->
-                bwait(kafkaAdminAPIDifferentOrganization.getAllTopics()));
+            bwait(kafkaAdminAPIDifferentOrganization.getAllTopics()));
     }
 
     @Test(timeOut = DEFAULT_TIMEOUT)
@@ -155,13 +123,13 @@ public class KafkaAdminAPITest extends TestBase {
         LOGGER.info("Test same organisation user");
         var bootstrapServerHost = kafka.bootstrapServerHost;
         var kafkaAdminAPISameOrganisationUser = bwait(KafkaAdminAPIUtils.kafkaAdminAPI(
-                vertx,
-                bootstrapServerHost,
-                Environment.SSO_SECONDARY_USERNAME,
-                Environment.SSO_SECONDARY_PASSWORD
+            vertx,
+            bootstrapServerHost,
+            Environment.SSO_SECONDARY_USERNAME,
+            Environment.SSO_SECONDARY_PASSWORD
         ));
         assertThrows(HTTPUnauthorizedException.class, () ->
-                bwait(kafkaAdminAPISameOrganisationUser.getAllTopics()));
+            bwait(kafkaAdminAPISameOrganisationUser.getAllTopics()));
     }
 
     @Test(timeOut = DEFAULT_TIMEOUT)
@@ -170,9 +138,9 @@ public class KafkaAdminAPITest extends TestBase {
         LOGGER.info("Test invalid token");
         var bootstrapServerHost = kafka.bootstrapServerHost;
         var apiURI = String.format("%s%s", Environment.KAFKA_ADMIN_API_SERVER_PREFIX, bootstrapServerHost);
-        KafkaAdminAPI kafkaAdminAPIUnauthorizedUser = new KafkaAdminAPI(vertx, apiURI, FAKE_TOKEN);
+        KafkaAdminAPI kafkaAdminAPIUnauthorizedUser = new KafkaAdminAPI(vertx, apiURI, TestUtils.FAKE_TOKEN);
         assertThrows(HTTPUnauthorizedException.class, () ->
-                bwait(kafkaAdminAPIUnauthorizedUser.getAllTopics()));
+            bwait(kafkaAdminAPIUnauthorizedUser.getAllTopics()));
     }
 
     @Test(timeOut = DEFAULT_TIMEOUT)
