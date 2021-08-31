fb66b10c92a1305310c584253bd701e00980fb5f
Davide Bizzarri <dbizzarr@redhat.com>
2021-08-31 13:57:57 +0200

    chore: add getting started quickstart scenario

--- src/test/java/io/managed/services/test/quickstarts/QuarkusApplicationTest.java
+++ src/test/java/io/managed/services/test/quickstarts/QuarkusApplicationTest.java
@@ -1,4 +1,4 @@
-package io.managed.services.test.quickstart;
+package io.managed.services.test.quickstarts;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
