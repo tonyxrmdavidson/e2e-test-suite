deb17961f681c317457724c7c172346c94c8c2c4
hzrncik <hzrncik@redhat.com>
2022-01-24 16:16:14 +0100

    new test class: kafka access control test

---
+++ src/test/java/io/managed/services/test/kafka/KafkaAccessControlTest.java
@@ -0,0 +1,9 @@
+package io.managed.services.test.kafka;
+
+public class KafkaAccessControlTest {
+    // TODO At least all described tests in the document are automated
+    //TODO KafkaMgmtAPIPermissionTest (migrate completely)
+    //TODO KafkaAdminPermissionTest (migrate completely)
+    //TODO KafkaInstanceAPITest (migrate all permission tests)
+
+}
