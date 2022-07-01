bd70d6ed1d0b6d0be74f3c5144011acdb17a394f
tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>
2022-07-01 20:18:32 +0100

    removed local for commands

--- scripts/run_e2e_integration_tests.sh
+++ scripts/run_e2e_integration_tests.sh
@@ -2,9 +2,9 @@
 
 echo "running E2E tests"
 
-local RESPONSE=$(curl -sXGET -H "Authorization: Bearer $(ocm token)" "https://$(oc get routes/kas-fleet-manager -n $NAMESPACE -o jsonpath='{.spec.host}')/api/kafkas_mgmt/v1/kafkas")
+RESPONSE=$(curl -sXGET -H "Authorization: Bearer $(ocm token)" "https://$(oc get routes/kas-fleet-manager -n $NAMESPACE -o jsonpath='{.spec.host}')/api/kafkas_mgmt/v1/kafkas")
 echo ${RESPONSE}
-local KAFKA_ID=$(echo ${RESPONSE} | jq -r .items[0].id)
+KAFKA_ID=$(echo ${RESPONSE} | jq -r .items[0].id)
 echo ${KAFKA_ID}
 
 
