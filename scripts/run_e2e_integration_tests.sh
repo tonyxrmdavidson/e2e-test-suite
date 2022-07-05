b8d9b40e55c867f67e3dae46cb8c655dbd2c9639
tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>
2022-07-05 13:52:37 +0100

    getting integration test suite running

--- scripts/run_e2e_integration_tests.sh
+++ scripts/run_e2e_integration_tests.sh
@@ -2,11 +2,5 @@
 
 echo "running E2E tests"
 
-RESPONSE=$(curl -sXGET -H "Authorization: Bearer $(ocm token)" "https://$(oc get routes/kas-fleet-manager -n $NAMESPACE -o jsonpath='{.spec.host}')/api/kafkas_mgmt/v1/kafkas")
-echo ${RESPONSE}
-KAFKA_ID=$(echo ${RESPONSE} | jq -r .items[0].id)
-echo ${KAFKA_ID}
-
-
-# mvn install -DskipTests
-# mvn verify -Pintegration
+mvn install -DskipTests
+mvn verify -Pintegration
