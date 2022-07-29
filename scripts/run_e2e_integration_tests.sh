3f99f3fbea332487f27be15af4a279f65108460f
tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>
2022-07-29 12:37:44 +0100

    adding export commands

--- scripts/run_e2e_integration_tests.sh
+++ scripts/run_e2e_integration_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "running E2E tests"
-
+export OPENSHIFT_API_URI="https://$(oc get routes --all-namespaces 2>&1 | grep -o -m1 'kas-fleet-manager-kas\S*')"
+echo $OPENSHIFT_API_URI
+export OPENSHIFT_IDENTITY_URI="https://sso.stage.redhat.com"
+echo OPENSHIFT_IDENTITY_URI
 # mvn install -DskipTests
 # mvn verify -Pintegration
