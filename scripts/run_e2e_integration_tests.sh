669cfd357c95687475bbbf79ebe8a0bd137f099e
tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>
2022-08-03 17:16:37 +0100

    fixing echo

--- scripts/run_e2e_integration_tests.sh
+++ scripts/run_e2e_integration_tests.sh
@@ -2,4 +2,4 @@
 
 echo "getting OPENSHIFT_API_URI"
 export OPENSHIFT_API_URI_VALUE="https://$(oc get routes --all-namespaces 2>&1 | grep -o -m1 'kas-fleet-manager-kas\S*')"
-echo $OPENSHIFT_API_URI
+echo $OPENSHIFT_API_URI_VALUE
