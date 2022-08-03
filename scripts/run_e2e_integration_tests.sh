bdd93dee44388cdf6db38946669e35172ddc0dfd
tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>
2022-08-03 16:34:36 +0100

    exporting var

--- scripts/run_e2e_integration_tests.sh
+++ scripts/run_e2e_integration_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "getting OPENSHIFT_API_URI"
-OPENSHIFT_API_URI_VALUE="https://$(oc get routes --all-namespaces 2>&1 | grep -o -m1 'kas-fleet-manager-kas\S*')"
+export OPENSHIFT_API_URI_VALUE="https://$(oc get routes --all-namespaces 2>&1 | grep -o -m1 'kas-fleet-manager-kas\S*')"
 echo $OPENSHIFT_API_URI
