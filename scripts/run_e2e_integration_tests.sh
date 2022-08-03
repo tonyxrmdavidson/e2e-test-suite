620859fd066b0ccc400b97fc5f9943fd1d700447
tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>
2022-08-03 11:46:39 +0100

    runnig tests and update of vars

--- scripts/run_e2e_integration_tests.sh
+++ scripts/run_e2e_integration_tests.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 echo "running E2E tests"
-export OPENSHIFT_API_URI="https://$(oc get routes --all-namespaces 2>&1 | grep -o -m1 'kas-fleet-manager-kas\S*')"
+OPENSHIFT_API_URI="https://$(oc get routes --all-namespaces 2>&1 | grep -o -m1 'kas-fleet-manager-kas\S*')"
 echo $OPENSHIFT_API_URI
-export OPENSHIFT_IDENTITY_URI="https://sso.stage.redhat.com"
+OPENSHIFT_IDENTITY_URI="https://sso.stage.redhat.com"
 echo $OPENSHIFT_IDENTITY_URI
-export LAUNCH_KEY="e2e-tony"
-echo $LAUNCH_KEY
-export REDHAT_SSO_URI="https://sso.redhat.com"
+REDHAT_SSO_URI="https://sso.redhat.com"
 echo $REDHAT_SSO_URI
-export SKIP_TEARDOWN="true"
-echo $SKIP_TEARDOWN
 echo $PRIMARY_USERNAME
-# mvn install -DskipTests
-# ./hack/testrunner.sh -p integration
+mvn install -DskipTests
+./hack/testrunner.sh -p integration
