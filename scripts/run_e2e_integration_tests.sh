6d40d5483036ee4a098a6144d62a199ab135c223
tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>
2022-08-04 18:30:07 +0100

    adding oc whoami to test if oc login is active

--- scripts/run_e2e_integration_tests.sh
+++ scripts/run_e2e_integration_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "getting OPENSHIFT_API_URI"
+# get kubeadmin pwd using ocm get
+# oc login with kubeadmin pwd
 export OPENSHIFT_API_URI_VALUE="https://$(oc get routes --all-namespaces 2>&1 | grep -o -m1 'kas-fleet-manager-kas\S*')"
 echo $OPENSHIFT_API_URI_VALUE
