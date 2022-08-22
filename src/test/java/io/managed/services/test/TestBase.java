c3cde7a356ca6b7752390fb55a4a4d57326f923c
hzrncik <hzrncik@redhat.com>
2022-08-23 01:17:58 +0200

    json encoding addition

--- src/test/java/io/managed/services/test/TestBase.java
+++ src/test/java/io/managed/services/test/TestBase.java
@@ -1,8 +1,11 @@
 package io.managed.services.test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.managed.services.test.framework.PrometheusSuiteListener;
 import io.managed.services.test.framework.PrometheusTestListener;
 import io.managed.services.test.framework.TestListener;
+import io.vertx.core.json.jackson.DatabindCodec;
 import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Listeners;
 
@@ -14,6 +17,12 @@ import org.testng.annotations.Listeners;
 public abstract class TestBase {
 
     static {
+        //  object mappers necessary for correct encode as JSON (global setting)
+        ObjectMapper mapper = DatabindCodec.mapper();
+        mapper.registerModule(new JavaTimeModule());
+        ObjectMapper prettyMapper = DatabindCodec.prettyMapper();
+        prettyMapper.registerModule(new JavaTimeModule());
+
         log.info("### Environment variables:");
         Environment.getValues().forEach((key, value) -> log.info("{}: {}", key, value));
 
